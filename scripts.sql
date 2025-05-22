c5ddafda4a678df48d6b8b44c78bb4316b94af28
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2025-05-22 15:02:42 +0300

    SQL

---
+++ scripts.sql
@@ -0,0 +1,23 @@
+first request
+SELECT *
+FROM student
+WHERE age BETWEEN 10 AND 20;
+
+second request
+SELECT name
+FROM student
+
+third request
+SELECT *
+FROM student
+WHERE name LIKE '%O%';
+
+fourth request
+SELECT *
+FROM student
+WHERE age < id;
+
+fifth request
+SELECT *
+FROM student
+ORDER BY age ASC;
