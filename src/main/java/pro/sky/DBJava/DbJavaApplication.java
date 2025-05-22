9790ff182411acc070518a83734117105cecb302
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2025-05-22 14:02:39 +0300

    Initial commit

---
+++ src/main/java/pro/sky/DBJava/DbJavaApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.DBJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbJavaApplication.class, args);
+	}
+
+}
